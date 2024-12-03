b1108a9264955a237521c6731aeb57b729524544
IGUHX <iguhinnocent10@gmail.com>
2024-12-03 11:34:44 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,33 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";  
+    dots[slideIndex - 1].className += " active";
+}
+
+// Optional: Automatically change slides every 3 seconds
+setInterval(() => {
+    plusSlides(1);
+}, 3000);
+
+document.getElementById("date").innerHTML="Date :"+ Date()
